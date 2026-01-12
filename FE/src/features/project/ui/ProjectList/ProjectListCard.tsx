91876c3f38942e2f93bc124f67a3d47e9375053a
이현빈 <lhb0107@naver.com>
2026-01-12 20:05:26 +0900

    ✨ feat: projectcard 맨 위 기수 표시 추가
    div를 새로 추가하여 absolute 적용

--- FE/src/features/project/ui/ProjectList/ProjectListCard.tsx
+++ FE/src/features/project/ui/ProjectList/ProjectListCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import paint from '../../../../../public/assets/paint.png';
 import Link from 'next/link';
+import TogglePill from '@/shared/ui/TogglePill';
 
 interface ProjectCardProps {
   project: {
@@ -22,15 +23,27 @@ const ProjectListCard = ({ project }: ProjectCardProps) => {
       className="rounded-xl bg-white shadow"
       scroll={false}
     >
-      <article className="rounded-xl bg-white shadow">
-        <Image
-          src={paint}
-          alt={project.title}
-          className="mb-4 rounded-tl-xl rounded-tr-xl"
-        />
-        <div className="flex flex-col gap-2 px-4 pb-2">
+      <article className="h-full overflow-hidden rounded-xl bg-white shadow">
+        <div className="relative w-full">
+          <Image
+            src={paint} // 실제로는 project.thumbnailUrl ?? paint 처럼 처리 필요할 수 있음
+            alt={project.title}
+            className="w-full object-cover"
+          />
+
+          <div className="absolute top-2 right-4 z-10">
+            <TogglePill
+              title={project.cohort.toString() + '기'}
+              isSelected={true}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 px-4 py-4">
           <span className="text-xl font-bold">{project.title}</span>
-          <span className="text-md text-gray-600">{project.description}</span>
+          <span className="text-md line-clamp-2 text-gray-600">
+            {project.description}
+          </span>
         </div>
       </article>
     </Link>
