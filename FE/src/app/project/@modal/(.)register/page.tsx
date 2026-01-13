20e13090080cfe6731aaadaaf549a767d818f17e
이현빈 <lhb0107@naver.com>
2026-01-13 18:54:36 +0900

    🐛 fix: project modal 부분 client -\> server, 버그 수정
    프로젝트 상세 모달 페이지에서 close button과 프로젝트 상세 부분을 밖으로 뺴서 해당 모달에만 뜨도록 변경

---
+++ FE/src/app/project/@modal/(.)register/page.tsx
@@ -0,0 +1,9 @@
+import ModalOverlay from '@/shared/ui/ModalOverlay';
+
+export default function ProjectRegisterPageModal() {
+  return (
+    <ModalOverlay>
+      <>프로젝트 등록 페이지임 근데 인터셉트한거임</>
+    </ModalOverlay>
+  );
+}
