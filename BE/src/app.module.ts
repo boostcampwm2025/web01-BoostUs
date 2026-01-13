2bb4207d05b8b23efd6ed357c66648878abc4cde
Jang-DongHo <donghoo18@naver.com>
2026-01-13 14:22:29 +0900

    🔧 chore: format 적용

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -3,11 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { StoryModule } from './story/story.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({
-    isGlobal: true,
-  })],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    StoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
