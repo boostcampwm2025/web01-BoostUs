ab00938aed1296577d6a799249441b75a1d5da03
LimSR <tmdfuf1201@naver.com>
2026-01-13 18:19:31 +0900

    feat: app.module 에 HttpExceptionFilter 등록 - 토큰 기반 Provider 등록 방식으로 등록했습니다.

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -3,12 +3,23 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filter/exception.filter';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({
-    isGlobal: true,
-  })],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
