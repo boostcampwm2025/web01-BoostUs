44c46559804bc2fe9815cbea0272865ac0c8fa5b
Seungjun Jeong <seungjun.jeong10@gmail.com>
2026-01-15 00:42:44 +0900

    💄 style: 랭킹 애니메이션 롤백

--- FE/src/features/stories/ui/StoriesRanking/Ranking.tsx
+++ FE/src/features/stories/ui/StoriesRanking/Ranking.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { motion, AnimatePresence } from 'framer-motion';
 import { useStoriesContext } from '@/features/stories/model';
 import StoriesRankingCard from '@/features/stories/ui/StoriesRanking/RankingCard';
 import StoriesRankingHeader from '@/features/stories/ui/StoriesRanking/RankingHeader';
@@ -12,28 +11,15 @@ const StoriesRanking = () => {
   const cards = Array.from({ length: 5 }, (_, index) => index);
 
   return (
-    <section className="border-neutral-border-default bg-neutral-surface-bold flex h-fit w-full flex-col overflow-hidden rounded-2xl border">
+    <section className="border-neutral-border-default bg-neutral-surface-bold flex h-fit w-full flex-col rounded-2xl border">
       <StoriesRankingHeader />
-      <AnimatePresence initial={false}>
-        {isRankingOpen && (
-          <motion.div
-            initial={{ height: 0, opacity: 0 }}
-            animate={{ height: 'auto', opacity: 1 }}
-            exit={{ height: 0, opacity: 0 }}
-            transition={{
-              height: { duration: 0.3, ease: 'easeInOut' },
-              opacity: { duration: 0.2, ease: 'easeInOut' },
-            }}
-          >
-            {cards.map((index) => (
-              <StoriesRankingCard
-                key={index}
-                hasBorder={index !== cards.length - 1}
-              />
-            ))}
-          </motion.div>
-        )}
-      </AnimatePresence>
+      {isRankingOpen &&
+        cards.map((index) => (
+          <StoriesRankingCard
+            key={index}
+            hasBorder={index !== cards.length - 1}
+          />
+        ))}
     </section>
   );
 };
