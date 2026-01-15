cf8af1b7b0b17a7bd9eca43b853fc0707d3c8f7d
LimSR <tmdfuf1201@naver.com>
2026-01-15 14:07:32 +0900

    ✨ feat: 백엔드 API 요청에 대해 /api prefix 추가
    - nginx 리버스 프록시를 위해 백엔드 api 요청은 prefix 를 추가했습니다

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -11,6 +11,8 @@ import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // app.setGlobalPrefix('api');
+
   // 응답 인터셉터 적용
   app.useGlobalInterceptors(new ResponseInterceptor());
 
