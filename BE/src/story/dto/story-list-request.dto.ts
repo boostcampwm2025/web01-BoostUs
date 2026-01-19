241d0652d51cf5c575e61e43d72d345a735c129e
Jang-DongHo <donghoo18@naver.com>
2026-01-19 14:03:15 +0900

    ⚡️ feat: StoryDto에 Swagger 데코레이터 추가

--- BE/src/story/dto/story-list-request.dto.ts
+++ BE/src/story/dto/story-list-request.dto.ts
@@ -1,3 +1,4 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsOptional } from 'class-validator';
 import { StoryPeriod, StorySortBy } from '../type/story-query.type';
 
@@ -5,10 +6,22 @@ import { StoryPeriod, StorySortBy } from '../type/story-query.type';
  * Story 목록 조회 요청 DTO
  */
 export class StoryListRequestDto {
+  @ApiPropertyOptional({
+    description: '정렬 기준',
+    enum: StorySortBy,
+    default: StorySortBy.LATEST,
+    example: StorySortBy.LATEST,
+  })
   @IsOptional()
   @IsEnum(StorySortBy)
   sortBy: StorySortBy = StorySortBy.LATEST;
 
+  @ApiPropertyOptional({
+    description: '집계 기간',
+    enum: StoryPeriod,
+    default: StoryPeriod.ALL,
+    example: StoryPeriod.ALL,
+  })
   @IsOptional()
   @IsEnum(StoryPeriod)
   period: StoryPeriod = StoryPeriod.ALL;
