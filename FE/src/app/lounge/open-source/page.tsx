f2817c3833b7bd68fe66f45c4808c97e332baa97
Seungjun Jeong <seungjun.jeong10@gmail.com>
2026-01-20 13:15:50 +0900

    🐛 fix: fe 빌드 오류 해결

--- FE/src/app/lounge/open-source/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function OpenSource() {
-  return <>오픈소스 기여 페이지</>;
-}
--- FE/src/entities/projectDetail/model/types.ts
+++ FE/src/entities/projectDetail/model/types.ts
@@ -1,4 +1,4 @@
-export interface  Participant {
+export interface Participant {
   githubId: string;
   avatarUrl?: string;
 }
