7af256b7d50c9cb2e4d2a29d8ee52efe752a0b67
JunHwan <devjunsday@gmail.com>
2026-01-21 19:41:34 +0900

    🐛 fix: interceptor 메세지 커스텀 가능하게 수정
    - 컨트롤러 에서 return 할 때 {message : \"메세지\" data: data} 형태로 응답하면됩니다.

--- BE/src/common/interceptor/response.interceptor.ts
+++ BE/src/common/interceptor/response.interceptor.ts
@@ -3,16 +3,47 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CommonResponseDto } from '../dto/common-response.dto';
 
+type WrappedResponse<T> = {
+  message?: string;
+  data?: T;
+};
+
+function isObject(x: unknown): x is Record<string, unknown> {
+  return typeof x === 'object' && x !== null;
+}
+
+function isWrappedResponse<T>(x: unknown): x is WrappedResponse<T> {
+  if (!isObject(x)) return false;
+
+  if ('message' in x && typeof x.message !== 'string') return false;
+
+  return true;
+}
+
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, CommonResponseDto<T>> {
   intercept(_: ExecutionContext, next: CallHandler<T>): Observable<CommonResponseDto<T>> {
     return next.handle().pipe(
-      map((data) => ({
-        success: true,
-        message: '성공적으로 응답되었습니다.',
-        error: null,
-        data,
-      })),
+      map((res: unknown) => {
+        const defaultMessage = '성공적으로 응답되었습니다.';
+
+        let message = defaultMessage;
+        let data: T;
+
+        if (isWrappedResponse<T>(res)) {
+          message = res.message ?? defaultMessage;
+          data = (res.data ?? res) as T; // res 자체가 data일 수도 있어서 fallback
+        } else {
+          data = res as T;
+        }
+
+        return {
+          success: true,
+          message,
+          error: null,
+          data,
+        };
+      }),
     );
   }
 }
