65e585e58d1ebddad200610b3fa48fc299849c60
LimSR <tmdfuf1201@naver.com>
2026-01-21 23:25:04 +0900

    ♻️ refactor: express 종속적인 Res 및 Response 사용을 Nest 내장 Redirect 데코레이터로 수정

--- BE/src/auth/auth.controller.ts
+++ BE/src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
-import type { Response } from 'express';
+import { Controller, Get, Query, Redirect } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GithubCallbackQueryDto } from './dto/github-callback-query.dto';
 
@@ -8,14 +7,17 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('login')
-  login(@Res() res: Response) {
+  @Redirect()
+  login() {
     const params = new URLSearchParams({
       client_id: process.env.GITHUB_CLIENT_ID ?? '',
       redirect_uri: process.env.GITHUB_REDIRECT_URI ?? '',
       scope: 'read:org',
     });
 
-    return res.redirect(`https://github.com/login/oauth/authorize?${params.toString()}`);
+    return {
+      url: `https://github.com/login/oauth/authorize?${params.toString()}`,
+    };
   }
 
   @Get('github/callback')
