a578554ba21988ce8704248dc5bb8687ff4a6564
LimSR <tmdfuf1201@naver.com>
2026-01-21 20:01:41 +0900

    ✨ feat: member 테이블 upsert 시 create 일 경우 랜덤 닉네임 저장 로직 추가
    - 랜덤 닉네임 생성 메서드를 service 레이어에 둘 지 repository 레이어에 둘 지 고민되었으나, default value 를 생성해 저장하는 흐름 상
    repository 레이어에 넣어도 될 것 같다고 판단하여 넣어두었습니다.

--- BE/src/auth/auth.repository.ts
+++ BE/src/auth/auth.repository.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GithubLoginUpsertDto } from './dto/github-login-upsert.dto';
+import { NICKNAME_ADJECTIVES, NICKNAME_NOUNS } from './type/nickname.type';
 
 @Injectable()
 export class AuthRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async upsertByGithubProfile(data: GithubLoginUpsertDto) {
+    const randomNickname = this._generateRandomNickname();
+
     return this.prisma.member.upsert({
       where: { githubId: data.githubId },
       update: {
@@ -17,7 +20,7 @@ export class AuthRepository {
       create: {
         githubId: data.githubId,
         githubLogin: data.githubLogin,
-        nickname: '',
+        nickname: randomNickname,
         avatarUrl: data.avatarUrl,
         cohort: data.cohort,
       },
@@ -31,5 +34,13 @@ export class AuthRepository {
         state: true,
       },
     });
+  }
+
+  private _generateRandomNickname(): string {
+    const adj = NICKNAME_ADJECTIVES[Math.floor(Math.random() * NICKNAME_ADJECTIVES.length)];
+    const noun = NICKNAME_NOUNS[Math.floor(Math.random() * NICKNAME_NOUNS.length)];
+    const suffix = Math.floor(1000 + Math.random() * 9000);
+
+    return `${adj}${noun}${suffix}`;
   }
 }
