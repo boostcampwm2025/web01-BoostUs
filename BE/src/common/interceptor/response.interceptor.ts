54fba4aa77a6512c954c2e85d32f30e362e2e4d0
JunHwan <devjunsday@gmail.com>
2026-01-22 12:52:05 +0900

    ♻️ refactor: 응답 메세지 데코코레이터 기반으로 변경

--- BE/src/common/interceptor/response.interceptor.ts
+++ BE/src/common/interceptor/response.interceptor.ts
@@ -1,49 +1,33 @@
+// common/interceptors/response.interceptor.ts
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CommonResponseDto } from '../dto/common-response.dto';
-
-type WrappedResponse<T> = {
-  message?: string;
-  data?: T;
-};
-
-function isObject(x: unknown): x is Record<string, unknown> {
-  return typeof x === 'object' && x !== null;
-}
-
-function isWrappedResponse<T>(x: unknown): x is WrappedResponse<T> {
-  if (!isObject(x)) return false;
-
-  if ('message' in x && typeof x.message !== 'string') return false;
-
-  return true;
-}
-
+import { RESPONSE_MESSAGE_KEY } from '../decorator/response-message.decorator';
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, CommonResponseDto<T>> {
-  intercept(_: ExecutionContext, next: CallHandler<T>): Observable<CommonResponseDto<T>> {
+  constructor(private readonly reflector: Reflector) {}
+
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<CommonResponseDto<T>> {
+    const handler = context.getHandler();
+    const cls = context.getClass();
+
+    // 메서드 > 클래스 순으로 override
+    const customMessage = this.reflector.getAllAndOverride<string>(RESPONSE_MESSAGE_KEY, [
+      handler,
+      cls,
+    ]);
+
+    const message = customMessage ?? '성공적으로 응답되었습니다.';
+
     return next.handle().pipe(
-      map((res: unknown) => {
-        const defaultMessage = '성공적으로 응답되었습니다.';
-
-        let message = defaultMessage;
-        let data: T;
-
-        if (isWrappedResponse<T>(res)) {
-          message = res.message ?? defaultMessage;
-          data = (res.data ?? res) as T; // res 자체가 data일 수도 있어서 fallback
-        } else {
-          data = res as T;
-        }
-
-        return {
-          success: true,
-          message,
-          error: null,
-          data,
-        };
-      }),
+      map((data) => ({
+        success: true,
+        message,
+        error: null,
+        data,
+      })),
     );
   }
 }
