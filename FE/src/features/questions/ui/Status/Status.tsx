cb194d3c42d9285906b06edcebb8656bca3d70ac
Seungjun Jeong <seungjun.jeong10@gmail.com>
2026-01-27 15:19:12 +0900

    💄 style: 메인 배경색 수정
    누런 느낌을 없앤 neutral grey로 변경

--- FE/src/features/questions/ui/Status/Status.tsx
+++ FE/src/features/questions/ui/Status/Status.tsx
@@ -4,13 +4,19 @@ const QuestionStatus = ({ status }: { status: boolean }) => {
   if (status) {
     return (
       <div className="flex flex-row items-center justify-center gap-1 px-2 py-1 rounded-lg w-fit bg-success-surface-default">
-        <CheckCircle className="text-accent-green" strokeWidth={2} size={14} />
-        <span className="text-accent-green text-string-12">해결됨</span>
+        <CheckCircle
+          className="text-brand-text-on-default"
+          strokeWidth={2}
+          size={14}
+        />
+        <span className="text-brand-text-on-default text-string-12">
+          해결됨
+        </span>
       </div>
     );
   } else {
     return (
-      <div className="flex flex-row items-center justify-center gap-1 px-2 py-1 rounded-lg w-fit bg-grayscale-300">
+      <div className="flex flex-row items-center justify-center gap-1 px-2 py-1 bg-brand-surface-weak rounded-lg w-fit bg-grayscale-300">
         <CircleQuestionMark
           className="text-neutral-text-weak"
           strokeWidth={2}
