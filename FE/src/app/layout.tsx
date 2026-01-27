9e464bb081aa1f822f8ad609586db9087f7ea2b6
이현빈 <lhb0107@naver.com>
2026-01-27 17:18:24 +0900

    ✨ feat: 마이페이지 1차 구현(나의 정보 section)
    1차로 변경되지 않은 나의 정보 section 구현

--- FE/src/app/layout.tsx
+++ FE/src/app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import Header from '@/widgets/Header';
 import Footer from '@/widgets/Footer';
-import { AuthProvider } from '@/features/login/model/auth.context';
+import { AuthInitializer } from '@/features/login/AuthInitializer';
 
 export const metadata: Metadata = {
   title: 'BoostUs - 부스트캠퍼들이 함께 기록하고, 함께 성장하는 커뮤니티 🌱',
@@ -25,13 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en" className={pretendard.variable}>
       <body className="flex flex-col min-h-screen font-sans antialiased">
-        <AuthProvider>
-          <Header />
-          <main className="bg-neutral-surface-default flex w-full flex-1 flex-col items-center px-4 py-32">
-            {children}
-          </main>
-          <Footer />
-        </AuthProvider>
+        <AuthInitializer />
+        <Header />
+        <main className="bg-neutral-surface-default flex w-full flex-1 flex-col items-center px-4 py-32">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
