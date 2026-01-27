246f904d83a00154f2ed420b026e4102725b4c04
Seungjun Jeong <seungjun.jeong10@gmail.com>
2026-01-27 14:53:13 +0900

    💄 style: 카드 UI 그림자 개선

--- FE/src/features/stories/ui/Card/Card.tsx
+++ FE/src/features/stories/ui/Card/Card.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { Eye, Heart } from 'lucide-react';
+import { Calendar1, Eye, Heart } from 'lucide-react';
 import Link from 'next/link';
 import type { Story } from '@/features/stories/model/stories.type';
 import useImageError from '@/shared/model/useImageError';
@@ -24,7 +24,7 @@ const StoriesCard = ({ id, story }: StoriesCardProps) => {
 
   return (
     <Link href={`/stories/${id}`}>
-      <div className="bg-neutral-surface-bold border-neutral-border-default hover:shadow-default grid w-full cursor-pointer grid-rows-[4fr_6fr] overflow-hidden rounded-2xl border transition-shadow duration-150">
+      <div className="bg-neutral-surface-bold border-neutral-border-default shadow-default hover:shadow-hover grid w-full cursor-pointer grid-rows-[4fr_6fr] overflow-hidden rounded-2xl border transition-shadow duration-150">
         <div className="relative w-full">
           <Image
             src={currentSrc}
@@ -47,24 +47,26 @@ const StoriesCard = ({ id, story }: StoriesCardProps) => {
           <div className="text-body-14 text-neutral-text-weak mt-2 mb-2 h-18 leading-6">
             <p className="line-clamp-3">{story.summary}</p>
           </div>
-          <div className="mt-3 flex w-full flex-row items-center justify-between">
-            <div className="flex flex-row items-center gap-2">
-              <div className="flex flex-row items-center gap-1">
-                <Heart className="text-neutral-text-weak h-3 w-3" />
-                <span className="text-body-12 text-neutral-text-weak">
-                  {story.likeCount}
-                </span>
-              </div>
-              <div className="flex flex-row items-center gap-1">
-                <Eye className="text-neutral-text-weak h-3 w-3" />
-                <span className="text-body-12 text-neutral-text-weak">
-                  {story.viewCount}
-                </span>
-              </div>
+          <div className="mt-3 flex w-full flex-row items-center gap-2">
+            <div className="flex flex-row items-center gap-1">
+              <Heart className="text-neutral-text-weak h-3 w-3" />
+              <span className="text-body-12 text-neutral-text-weak">
+                {story.likeCount}
+              </span>
             </div>
-            <span className="text-body-12 text-neutral-text-weak">
-              {extractDate(story.createdAt)}
-            </span>
+            <div className="flex flex-row items-center gap-1">
+              <Eye className="text-neutral-text-weak h-3 w-3" />
+              <span className="text-body-12 text-neutral-text-weak">
+                {story.viewCount}
+              </span>
+            </div>
+
+            <div className="flex flex-row items-center gap-1">
+              <Calendar1 className="text-neutral-text-weak h-3 w-3" />
+              <span className="text-body-12 text-neutral-text-weak">
+                {extractDate(story.createdAt)}
+              </span>
+            </div>
           </div>
         </div>
       </div>
