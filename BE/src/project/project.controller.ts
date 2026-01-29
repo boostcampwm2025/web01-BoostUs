afd1f86f822378276bdcd65a64a87827ce68456f
LimSR12 <tmdfuf1201@naver.com>
2026-01-29 14:35:54 +0900

    ✨ feat: 프로젝트 상세조회 시 ViewerKey 데코레이터 사용하도록 변경

--- BE/src/project/project.controller.ts
+++ BE/src/project/project.controller.ts
@@ -12,6 +12,7 @@ import {
   UseInterceptors,
   Req,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -25,6 +26,8 @@ import { ProjectService } from './project.service';
 import { CurrentMember } from 'src/auth/decorator/current-member.decorator';
 import type { Request, Response } from 'express';
 import { randomUUID } from 'node:crypto';
+import { ViewerKeyGuard } from 'src/view/guard/view.guard';
+import { ViewerKey } from 'src/view/decorator/viewer-key.decorator';
 
 @ApiTags('프로젝트')
 @Controller('projects')
@@ -57,6 +60,7 @@ export class ProjectController {
   }
 
   @Public()
+  @UseGuards(ViewerKeyGuard)
   @Get(':id')
   @ApiOperation({
     summary: '프로젝트 상세 조회',
@@ -76,20 +80,7 @@ export class ProjectController {
     status: 404,
     description: '프로젝트를 찾을 수 없음',
   })
-  findOne(
-    @Param('id', ParseIntPipe) id: number,
-    @Req() req: Request,
-    @Res({ passthrough: true }) res: Response,
-  ) {
-    let viewerKey = req.cookies?.bid as string | undefined;
-    if (!viewerKey) {
-      viewerKey = randomUUID();
-      res.cookie('bid', viewerKey, {
-        httpOnly: true,
-        sameSite: 'lax',
-        secure: process.env.NODE_ENV === 'production',
-      });
-    }
+  findOne(@Param('id', ParseIntPipe) id: number, @ViewerKey() viewerKey: string) {
     return this.projectService.findOneWithViewCount(id, viewerKey);
   }
 
