6cb806136c3b06dd8fc5b2be752bd602a65e10c8
이현빈 <lhb0107@naver.com>
2026-01-31 14:38:37 +0900

    ✨ feat: login / MyPage 코드 spliting
    Container 추가 하여 검증

--- FE/src/app/login/page.tsx
+++ FE/src/app/login/page.tsx
@@ -1,5 +1,11 @@
-import LoginBtn from '@/features/login/LoginBtn';
+import type { Metadata } from 'next';
+import LoginContainer from '@/widgets/Login/Login.container'; // 분리한 클라이언트 컴포넌트
+
+export const metadata: Metadata = {
+  title: '로그인 - BoostUs',
+  description: '로그인하고 서비스를 이용해보세요.',
+};
 
 export default function LoginPage() {
-  return <LoginBtn />;
+  return <LoginContainer />;
 }
