92db8c197ed64c3473b16c64984399375b612e36
Jang-DongHo <donghoo18@naver.com>
2026-02-02 13:45:54 +0900

    🔧 chore: auth 모듈 lint 에러 및 warning 수정

--- BE/src/auth/decorator/public.decorator.ts
+++ BE/src/auth/decorator/public.decorator.ts
@@ -1,4 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
+// 데코레이터 이름은 @Public() 사용을 위해 PascalCase 유지
+// eslint-disable-next-line @typescript-eslint/naming-convention -- decorator API
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
