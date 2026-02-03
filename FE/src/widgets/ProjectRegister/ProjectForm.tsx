9e6e8babd02bd06fe51f3b175af7fb1d83e1a551
LimSR <tmdfuf1201@naver.com>
2026-02-03 23:41:38 +0900

    ♻️ refactor: 불필요한 toString() 제거 (lint 에러)

--- FE/src/widgets/ProjectRegister/ProjectForm.tsx
+++ FE/src/widgets/ProjectRegister/ProjectForm.tsx
@@ -105,7 +105,7 @@ export default function ProjectForm({ projectId }: ProjectFormProps) {
 
   const openRepoModal = () => {
     setRepoError(null);
-    setRepoUrlInput((repoUrlValue ?? '').toString());
+    setRepoUrlInput(repoUrlValue ?? '');
     setIsRepoModalOpen(true);
   };
 
