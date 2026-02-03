11b6becc1204a41b483dc1154d0d7fb49cf7416f
Jang-DongHo <donghoo18@naver.com>
2026-02-03 23:41:49 +0900

    🔥 remove: 사용되지 않는 IsAdmin 데코레이터 삭제

--- BE/src/auth/guard/admin.guard.ts
+++ BE/src/auth/guard/admin.guard.ts
@@ -1,8 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { Role } from '../../generated/prisma/enums';
-import { IS_ADMIN_KEY } from '../decorator/admin.decorator';
 import { AdminRequiredException } from '../exception/auth.exception';
 
 interface RequestWithMember extends Request {
@@ -11,18 +9,7 @@ interface RequestWithMember extends Request {
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
-
   canActivate(context: ExecutionContext): boolean {
-    const isAdmin = this.reflector.getAllAndOverride<boolean>(IS_ADMIN_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    if (!isAdmin) {
-      return true;
-    }
-
     const request = context.switchToHttp().getRequest<RequestWithMember>();
     const member = request.member;
 
