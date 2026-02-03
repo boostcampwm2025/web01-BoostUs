afe7f1f45d94d17b14275b62e37850b6967a1a94
Seungjun Jeong <seungjun.jeong10@gmail.com>
2026-02-03 21:47:36 +0900

    ♻️ refactor: 추천 프로젝트 섹션 useQuery 리팩토링

--- FE/src/features/main/reco/api/fetchRecoProject.ts
+++ FE/src/features/main/reco/api/fetchRecoProject.ts
@@ -2,6 +2,8 @@ import { Project } from '@/features/project/api/getProjects';
 import { ApiResponse } from '@/shared/types/ApiResponseType';
 import { customFetch } from '@/shared/utils/fetcher';
 
+export const RECO_PROJECT_QUERY_KEY = ['reco-projects'];
+
 export const fetchRecoProject = async (): Promise<ApiResponse<Project[]>> => {
   return customFetch<ApiResponse<Project[]>>(`/api/recommend`);
 };
