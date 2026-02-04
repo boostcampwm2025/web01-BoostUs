b5b8bb3678ae3bbbe4eba0f998b39737c7446914
LimSR <tmdfuf1201@naver.com>
2026-02-04 23:05:39 +0900

    🐛 fix: 받아오는 feed.id 를 string 으로 수정함에 따라 api 호출 메서드에서도 string id 받도록 수정

--- FE/src/features/feed/api/feed.api.ts
+++ FE/src/features/feed/api/feed.api.ts
@@ -2,9 +2,9 @@ import { ApiResponse } from '@/shared/types/ApiResponseType';
 import { customFetch } from '@/shared/utils/fetcher';
 
 export interface FeedDetail {
-  id: number;
+  id: string;
   feedUrl: string;
-  memberId: number;
+  memberId: string;
   state: 'ACTIVE' | 'INACTIVE';
   createdAt: string;
   lastFetchedAt: string | null;
@@ -28,13 +28,10 @@ export const createOrUpdateFeed = async (body: { feedUrl: string }) => {
   return response.data;
 };
 
-export const deleteFeed = async (id: number) => {
-  const response = await customFetch<ApiResponse<null>>(
-    `/api/feeds/${id.toString()}`,
-    {
-      method: 'DELETE',
-    }
-  );
+export const deleteFeed = async (id: string) => {
+  const response = await customFetch<ApiResponse<null>>(`/api/feeds/${id}`, {
+    method: 'DELETE',
+  });
 
   return response.data;
 };
