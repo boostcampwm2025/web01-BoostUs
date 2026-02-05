7c3688530d2b1eff536e1c76c0229a3b0fc43476
Jang-DongHo <donghoo18@naver.com>
2026-02-05 15:10:25 +0900

    🐛 fix: 마이페이지 및 닉네임 변경 버그 수정

--- FE/src/features/myPage/api/updateNickname.ts
+++ FE/src/features/myPage/api/updateNickname.ts
@@ -2,8 +2,11 @@ import { ApiResponse } from '@/shared/types/ApiResponseType';
 import { customFetch } from '@/shared/utils/fetcher';
 
 export async function updateNickname(nickname: string) {
-  await customFetch<ApiResponse<void>>('/api/members/me/profile/nickname', {
+  await customFetch<ApiResponse<unknown>>('/api/members/me/profile/nickname', {
     method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({ nickname }),
     credentials: 'include',
   });
