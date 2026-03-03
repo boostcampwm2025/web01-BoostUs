e275668c111282e00baeae19ce5376383e2d692b
LimSR <tmdfuf1201@naver.com>
2026-03-03 19:08:31 +0900

    🔧 chore: 스웨거 설정 분리
    - main.ts 를 가볍게 만들기 위해 스웨거 설정을 config/swagger/ 로 분리하였습니다.

---
+++ BE/src/config/swagger/swagger.config.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('boostus')
+    .setDescription('boostus API 명세서')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/api-docs', app, document, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: 5,
+      defaultModelExpandDepth: 5,
+    },
+  });
+}
