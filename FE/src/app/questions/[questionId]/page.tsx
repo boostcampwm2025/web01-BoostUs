47c751ae4011e8d3459892402b172918c4d399d5
Jang-DongHo <donghoo18@naver.com>
2026-03-30 19:14:58 +0900

    🐛 hotfix: dynamic-server-error 에러 우회 코드 추가

--- FE/src/app/questions/[questionId]/page.tsx
+++ FE/src/app/questions/[questionId]/page.tsx
@@ -10,6 +10,7 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 
+export const dynamic = 'force-dynamic';
 export const revalidate = 300;
 export const dynamicParams = true;
 
--- FE/src/app/stories/[id]/page.tsx
+++ FE/src/app/stories/[id]/page.tsx
@@ -14,6 +14,7 @@ interface Props {
   params: Promise<{ id: string }>;
 }
 
+export const dynamic = 'force-dynamic';
 export const revalidate = 3600;
 export const dynamicParams = true;
 
